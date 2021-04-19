92efe8a0fb4a412618b7d25413cff9d38c6d4ebb
pranaypiyush25 <51981995+pranaypiyush25@users.noreply.github.com>
2021-04-19 23:58:01 +0530

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,13 +1,12 @@
 import * as firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA5bntlk_2hKdyWp327_0JNgiqoPlMypCw",
-    authDomain: "ipl-auction-bphc-1570b.firebaseapp.com",
-    databaseURL: "https://ipl-auction-bphc-1570b-default-rtdb.firebaseio.com",
-    projectId: "ipl-auction-bphc-1570b",
-    storageBucket: "ipl-auction-bphc-1570b.appspot.com",
-    messagingSenderId: "278418584038",
-    appId: "1:278418584038:web:f4bc9e8c41a2c05eb26627"
+     apiKey: "AIzaSyB0SnGJSYEyPlFn0cR4-p4nOBLtU4qR-d4",
+    authDomain: "ipl-auction-bphc-809e9.firebaseapp.com",
+    projectId: "ipl-auction-bphc-809e9",
+    storageBucket: "ipl-auction-bphc-809e9.appspot.com",
+    messagingSenderId: "445746092559",
+    appId: "1:445746092559:web:fe0405847f514cbc8a617a"
   };
 
   firebase.initializeApp(firebaseConfig);
